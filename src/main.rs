ecbe27caa32e3d88ada4cc5b9b3f8f315c9451ab
ibrahimgb <ibrahim.guoual.b@gmail.com>
2024-01-15 21:25:42 +0100

    add threads and a ip list

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,34 @@
 #![allow(unused)]
 use std::net::TcpStream;
+use std::thread;
 fn main() {
-    use std::net::TcpStream;
     println!("Hello, world!");
-    can_be_reached("192.168.1.1",80)
+
+    let addresses = [
+        "192.168.31.1",
+        "192.168.31.15",
+        "192.168.31.23",
+        "192.168.31.68",
+        "192.168.31.107",
+        "192.168.31.123",
+        "192.168.31.178",
+        "192.168.31.202",
+    ];
+    
+    let handle = thread::spawn(move || {
+        for ip in addresses {
+        println!("{}",ip);
+
+        can_be_reached(ip,80);
+
+    }
+    });
+
+
+    handle.join().unwrap();
+
+    // can_be_reached("192.168.1.100",80);
+    
 }
 
 fn can_be_reached(ip: &str , port: i32) {
