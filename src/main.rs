27384619490d8c2d84f9ea2ec68a2ca80993e9b7
ibrahimgb <ibrahim.guoual.b@gmail.com>
2024-01-15 21:01:31 +0100

    add connection test function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+#![allow(unused)]
+use std::net::TcpStream;
 fn main() {
+    use std::net::TcpStream;
     println!("Hello, world!");
+    can_be_reached("192.168.1.1",80)
+}
+
+fn can_be_reached(ip: &str , port: i32) {
+    if let Ok(stream) = TcpStream::connect(ip.to_string() +":"+&port.to_string()) {
+    println!("the address has been reached");
+} else {
+    println!("Couldn't reached the address...");
+}
 }
